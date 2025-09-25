1d39e809b6f949dcf900237605ec3dd45d284421
kdorcelien <kdorcelien.kd@gmail.com>
2025-09-25 10:06:52 -0400

    Final commit

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -1,7 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class SandwichShop {
     public static void main(String[] args) {
+        Scanner sandwich = new Scanner(System.in);
 
+        System.out.print("available sandwich size: \n A: Regular: base price $5.45 \n B: Large: base price $8.95 \n ");
+        System.out.print(" Please select a size: ");
+        String size = sandwich.nextLine();
+
+        System.out.println("Please enter age: ");
+        int age = sandwich.nextInt();
+
+        if (age <= 17) {
+            System.out.print(" You get a 10% discount");
+
+        } else if (age >= 65) {
+            System.out.print("You get a 20% discount");
+        }
     }
 }
